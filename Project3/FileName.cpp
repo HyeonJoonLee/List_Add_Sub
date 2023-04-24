c0703658e93e62b21b9ef8a1d190f00df4757756
HyeonJoon Lee <lhj1247416@gmail.com>
2023-04-25 01:31:50 +0900

    d

--- Project3/FileName.cpp
+++ Project3/FileName.cpp
@@ -1,4 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
@@ -29,6 +28,14 @@ typedef struct student2
 
 
 //함수 선언
+void score(int k[], int e[], int m[], int t[], float a[])
+{
+	for (int i = 0; i < 3; i++)
+	{
+		t[i] = k[i] + e[i] + m[i];
+		a[i] = t[i] / 3.0;
+	}
+}
 //다항식 덧셈하는 함수
 void add(poly1* PA, poly1* PB, poly1* PC)
 {
@@ -91,21 +98,18 @@ void sub(poly1* PA, poly1* PB, poly1* PD)
 	}
 }
 
-//다항식 덧뺄셈 두번째방법
 void calc(poly2 P[])
 {
 	int i, n;
 	int As, Bs, Cs, Ae, Be;
 
 	//입력
-	std::cout << "\n다항식 A의 항 개수 : "; std::cin >> n;
+	std::cout << "다항식 A의 항 개수 : "; std::cin >> n;
 	As = 0, Ae = As + n - 1;
-	std::cout << "\n다항식 A의 항과 차수를 입력하세요 \n: ";
 	for (i = As; i <= Ae; i++)
 		scanf_s("%f %d", &P[i].coef, &P[i].exp);
 
-	std::cout << "\n다항식 B의 항 개수 : "; std::cin >> n;
-	std::cout << "\n다항식 B의 항과 차수를 입력하세요 \n: ";
+	std::cout << "다항식 B의 항 개수 : "; std::cin >> n;
 	Bs = Ae + 1, Be = Bs + n - 1;
 	for (i = Bs; i <= Be; i++)
 		scanf_s("%f %d", &P[i].coef, &P[i].exp);
@@ -155,20 +159,30 @@ void calc(poly2 P[])
 		}
 
 	//출력
-	std::cout << "\n\n 다항식 A = ";
+	std::cout << "\n 다항식 A = ";
 	for (i = 0; i <= Ae; i++)
-		printf(" %.1fX%d ", P[i].coef, P[i].exp);
+		printf(" %6.1fX%d ", P[i].coef, P[i].exp);
 
-	std::cout << "\n\n 다항식 B = ";
+	std::cout << "\n 다항식 B = ";
 	for (i = Ae + 1; i <= Be; i++)
-		printf(" %.1fX%d ", P[i].coef, P[i].exp);
+		printf(" %6.1fX%d ", P[i].coef, P[i].exp);
 
-	std::cout << "\n\n 다항식 C (덧셈) = ";
+	std::cout << "\n 다항식 C = ";
 	for (i = Be + 1; i <= Cs - 1; i++)
-		printf(" %.1fX%d ", P[i].coef, P[i].exp);
+		printf(" %6.1fX%d ", P[i].coef, P[i].exp);
 
 
 
+}
+
+void score2(student sss[])
+{
+	int i;
+	for (i = 0; i < 3; i++)
+	{
+		sss[i].tot = sss[i].k + sss[i].e + sss[i].m;
+		sss[i].ave = sss[i].tot / 3.0;
+	}
 }
 
 poly3* link_add(poly3* X, poly3* Y, poly3* Z)  //연결 구조체를 이용한 다항식 덧셈 함수
@@ -215,7 +229,6 @@ poly3* link_add(poly3* X, poly3* Y, poly3* Z)  //연결 구조체를 이용한 다항식 덧�
 			r = (poly3*)malloc(sizeof(struct poly3));
 			q = q->link;
 		}
-		r2->link = NULL;
 	}
 	else if (q == NULL)
 	{
@@ -226,69 +239,8 @@ poly3* link_add(poly3* X, poly3* Y, poly3* Z)  //연결 구조체를 이용한 다항식 덧�
 			r = (poly3*)malloc(sizeof(struct poly3));
 			p = p->link;
 		}
-		r2->link = NULL;
 	}
 	return Z;
-}
-
-poly3* link_sub(poly3* X, poly3* Y, poly3* W)  //연결 구조체를 이용한 다항식 뺄셈 함수
-{
-	poly3* p, * q, * r, * r2;
-	p = X, q = Y;
-	r = (poly3*)malloc(sizeof(struct poly3));
-	W = r2 = r;
-
-	while (1)
-	{
-		if (p == NULL || q == NULL) break;
-		if (p->exp == q->exp) //차수가 같으면?
-		{
-			if (p->coef != q->coef)
-			{
-				r->coef = p->coef - q->coef; r->exp = p->exp;
-				r->link = NULL;  r2->link = r; r2 = r;
-				r = (poly3*)malloc(sizeof(struct poly3));
-			}
-			p = p->link;  q = q->link;
-		}
-		else if (p->exp > q->exp)  //다항식 X의 차수가 더 크면?
-		{
-			r->coef = p->coef; r->exp = p->exp;
-			r->link = NULL; r2->link = r;  r2 = r;
-			r = (poly3*)malloc(sizeof(struct poly3));
-			p = p->link;
-		}
-		else if (p->exp < q->exp)  //다항식 Y의 차수가 더 크면? 
-		{
-			r->coef = q->coef; r->exp = q->exp;
-			r->link = NULL; r2->link = r;  r2 = r;
-			r = (poly3*)malloc(sizeof(struct poly3));
-			q = q->link;
-		}
-	}//화일문 끝 --> 남은 항들을 W에 붙여넣기
-	if (p == NULL)
-	{
-		while (q != NULL)
-		{
-			r->coef = q->coef; r->exp = q->exp;
-			r->link = NULL;  r2->link = r;  r2 = r;
-			r = (poly3*)malloc(sizeof(struct poly3));
-			q = q->link;
-		}
-		r2->link = NULL;
-	}
-	else if (q == NULL)
-	{
-		while (p != NULL)
-		{
-			r->coef = p->coef; r->exp = p->exp;
-			r->link = NULL;  r2->link = r;  r2 = r;
-			r = (poly3*)malloc(sizeof(struct poly3));
-			p = p->link;
-		}
-		r2->link = NULL;
-	}
-	return W;
 }
 
 void main()
@@ -307,16 +259,36 @@ void main()
 	student2* s, * p, * q;
 	s = q = p = NULL;
 
-	poly3* X, * Y, * Z, * W, * a1, * b1, * c1, * c2;
-	X = Y = Z = W = a1 = b1 = c1 = c2 = NULL;
+	poly3* X, * Y, * Z, * a1, * b1, * c1, * c2;
+	X = Y = Z = a1 = b1 = c1 = c2 = NULL;
 
 	while (1) //무한루프
 	{
-		printf("\n\n\n 메뉴 1)다항식-1 덧뺄셈  2)다항식-2 덧뺄셈  3)다항식 덧뺄셈(연결구조체)   4)종료: ");
+		printf("\n\n\n 메뉴 1)성적처리(배열)  2)다항식-1 덧뺄셈  3)다항식-2 덧뺄셈");
+		printf("4)성적처리  5)성적처리(연결구조체) 6)다항식 덧뺄셈(연결구조체)   7)     8)종료: ");
 		scanf_s("%d", &menu);
-		if (menu == 4) break;
+		if (menu == 8) break;
 		switch (menu) {
-		case 1://다항식 2개 입력, 계산, 결과 다항식 출력
+		case 1://학번, 이름, 국,영,수 입력해서 총점/평균 계산후 출력
+			printf("학번, 이름, 국, 영, 수 입력 \n");
+			for (i = 0; i < 3; i++)
+			{
+				printf("\n 학번 : "); scanf_s("%d", &no[i]);
+				printf("\n 이름 : "); std::cin >> name[i];
+				printf("\n 국어 : "); scanf_s("%d", &k[i]);
+				printf("\n 영어 : "); scanf_s("%d", &e[i]);
+				printf("\n 수학 : "); scanf_s("%d", &m[i]);
+				printf("\n");
+			}
+			score(k, e, m, t, a);
+			printf("\n 학번, 이름, 국, 영, 수, 총점, 평균  \n");
+			for (i = 0; i < 3; i++)
+			{
+				printf(" %3d %10s %3d %3d %3d %3d %5.1f \n",
+					no[i], name[i], k[i], e[i], m[i], t[i], a[i]);
+			}
+			break;
+		case 2://다항식 2개 입력, 계산, 결과 다항식 출력
 			printf("\n 다항식 A의 차수 : "); scanf_s("%d", &A.degree);
 			printf("\n 다항식 A의 계수 : ");
 			for (i = 0; i <= A.degree; i++)
@@ -332,51 +304,101 @@ void main()
 			sub(&A, &B, &D);
 
 			//출력
-			printf("\n\n 다항식 A = ");
+			printf("\n 다항식 A = ");
 			for (i = 0; i <= A.degree; i++)
 				printf("%.1fX%d + ", A.coef[i], A.degree - i);
-			printf("\n\n 다항식 B = ");
+			printf("\n 다항식 B = ");
 			for (i = 0; i <= B.degree; i++)
 				printf("%.1fX%d + ", B.coef[i], B.degree - i);
-			printf("\n\n 덧셈한 다항식 C (덧셈) = ");
+			printf("\n 덧셈한 다항식 C = ");
 			for (i = 0; i <= C.degree; i++)
 				printf("%.1fX%d + ", C.coef[i], C.degree - i);
-			printf("\n\n 뺄셈한 다항식 D (뺄셈) = ");
+			printf("\n 뺄셈한 다항식 D = ");
 			for (i = 0; i <= D.degree; i++)
 				printf("%.1fX%d + ", D.coef[i], D.degree - i);
 			break;
-		case 2://poly2 구조체를 이용하여 다항식 덧뺄셈 수행
+		case 3://poly2 구조체를 이용하여 다항식 덧뺄셈 수행
 			calc(P);
 			break;
-		case 3://연결 구조체를 이용하여 다항식 덧뺄셈 하기
+		case 4:
+			printf("학번, 이름, 국, 영, 수 입력 \n");
+			for (i = 0; i < 3; i++)
+			{
+				printf("\n 학번 : "); std::cin >> st[i].no;
+				printf("\n 이름 : "); std::cin >> st[i].name;
+				printf("\n 국어 : "); std::cin >> st[i].k;
+				printf("\n 영어 : "); std::cin >> st[i].e;
+				printf("\n 수학 : "); std::cin >> st[i].m;
+				printf("\n");
+			}
+			score2(st);
+
+			printf("\n 학번, 이름, 국, 영, 수, 총점, 평균  \n");
+			for (i = 0; i < 3; i++)
+			{
+				printf(" %3d %10s %3d %3d %3d %3d %5.1f \n",
+					st[i].no, st[i].name, st[i].k, st[i].e,
+					st[i].m, st[i].tot, st[i].ave);
+			}
+			break;
+		case 5://자기참조 구조체로 성적처리
+			p = (student2*)malloc(sizeof(student2));
+			s = q = p;
+			while (1)
+			{
+				printf("\n 학번 : "); std::cin >> p->no;
+				if (p->no < 0) break;
+				printf("\n 이름 : "); std::cin >> p->name;
+				printf("\n 국어 : "); std::cin >> p->k;
+				printf("\n 영어 : "); std::cin >> p->e;
+				printf("\n 수학 : "); std::cin >> p->m;
+				p->tot = p->k + p->e + p->m;
+				p->ave = p->tot / 3.0;
+				// 입력이 연속적으로 진행될 경우를 위한 연속 동작 3가지 
+				p->link = NULL; //마지막 구조체는 후속 구조체가 없으므로 null로 처리 
+				q->link = p;    // 방금 생성한 구조체 주소 저장
+				q = p;          //q는 p를 따라감
+				p = (student2*)malloc(sizeof(student2)); //구조체 공간 할당
+			}
+
+			//출력
+			p = s;
+			while (1)
+			{
+				if (p == NULL) break;
+				printf("\n %3d %10s %3d %3d %3d %3d %7.1f",
+					p->no, p->name, p->k, p->e, p->m, p->tot, p->ave);
+				p = p->link;
+			}
+			break;
+		case 6://연결 구조체를 이용하여 다항식 덧뺄셈 하기
 			//다항식 X 입력
-			std::cout << "\n 다항식 A 입력(계수 차수 : 계수 -99 입력하면 종료)";
+			std::cout << "\n 다항식 A 입력(계수 차수 : 계수 -999 입력하면 종료)";
 			a1 = (poly3*)malloc(sizeof(struct poly3));
 			X = b1 = a1;
 			while (1)
 			{
 				std::cout << "\n 계수 : ";  std::cin >> a1->coef;
-				if (a1->coef == -99) break;
+				if (a1->coef == -999) break;
 				std::cout << "\n 차수 : ";  std::cin >> a1->exp;
 				a1->link = NULL;  b1->link = a1;  b1 = a1;
 				a1 = (poly3*)malloc(sizeof(struct poly3));
 			}
 
 			//다항식 Y 입력
-			std::cout << "\n 다항식 B 입력(계수 차수 : 계수 -99 입력하면 종료)";
+			std::cout << "\n 다항식 B 입력(계수 차수 : 계수 -999 입력하면 종료)";
 			a1 = (poly3*)malloc(sizeof(struct poly3));
 			Y = b1 = a1;
 			while (1)
 			{
 				std::cout << "\n 계수 : ";  std::cin >> a1->coef;
-				if (a1->coef == -99) break;
+				if (a1->coef == -999) break;
 				std::cout << "\n 차수 : ";  std::cin >> a1->exp;
 				a1->link = NULL;  b1->link = a1;  b1 = a1;
 				a1 = (poly3*)malloc(sizeof(struct poly3));
 			}
 			//계산
 			Z = link_add(X, Y, Z);
-			W = link_sub(X, Y, W);
 
 			//다항식 X 출력
 			a1 = X;
@@ -396,40 +418,14 @@ void main()
 				std::cout << a1->coef << "X" << a1->exp << "  ";
 				a1 = a1->link;
 			}
-
+			//다항식 Z 출력
+			a1 = Z;
+			std::cout << "\n\n 다항식 C = ";
 			while (1)
 			{
-				std::cout << "\n\n 1) 덧셈 2) 뺄셈 3) 종료 : ";
-				std::cin >> i;
-				if (i == 1)
-				{
-					//덧셈 다항식 Z 출력
-					std::cout << "\n\n 다항식 C = ";
-					a1 = Z;
-					while (1)
-					{
-
-						if (a1 == NULL) break;
-						std::cout << a1->coef << "X" << a1->exp << "  ";
-						a1 = a1->link;
-					}
-					break;
-				}
-				else if (i == 2)
-				{
-					//뺄셈 다항식 W 출력
-					std::cout << "\n\n 다항식 D = ";
-					a1 = W;
-					while (1)
-					{
-
-						if (a1 == NULL) break;
-						std::cout << a1->coef << "X" << a1->exp << "  ";
-						a1 = a1->link;
-					}
-					break;
-				}
-				else if (i == 3) break;
+				if (a1 == NULL) break;
+				std::cout << a1->coef << "X" << a1->exp << "  ";
+				a1 = a1->link;
 			}
 
 			break;
